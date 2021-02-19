57f2e1c9900d3898eeaf7af7ca08461117c687d3
meg4ik <meg4mag@gmail.com>
2021-02-19 14:21:26 +0200

    new layout and requirements

--- info_list/my_app/models.py
+++ info_list/my_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Searchs(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.search
+
+    # just rename this model
+    class Meta:
+        verbose_name_plural = 'Searches'
--- info_list/my_app/views.py
+++ info_list/my_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 
 def home(request):
     return render(request, 'base.html')
+
+def new_searh(request):
+    reqw = request.POST.get("search")
+    content = {'search': reqw,}
+    return render(request, 'my_app/new_search.html', content)
